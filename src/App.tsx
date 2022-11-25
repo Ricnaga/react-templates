05d267352d7747acd66737cb4aa5da7a5bd9adce
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-11-24 21:08:34 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from "formik";
+import React from "react";
 
-function App() {
+export function App() {
+  const formik = useFormik({
+    initialValues: {
+      user: "",
+      password: "",
+    },
+    onSubmit: (values) => console.log(values),
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          placeholder="USER"
+          name="user"
+          onChange={formik.handleChange}
+        />
+        <input
+          type="text"
+          placeholder="PASSWORD"
+          name="password"
+          onChange={formik.handleChange}
+        />
+        <button type="submit">ENVIAR</button>
+      </form>
+    </>
   );
 }
-
-export default App;
