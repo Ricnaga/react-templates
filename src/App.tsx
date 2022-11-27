dcc357d85b2fd7c7369a25e6883a110b5ba2c6ef
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-11-27 10:20:20 -0300

    feat: Add Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { RouterContext } from "./application/routes/context";
+import { TopBar } from "./shared/components/TopBar/TopBar";
 
 export function App() {
-  return <RouterContext />;
+  return (
+    <RouterContext>
+      <TopBar />
+    </RouterContext>
+  );
 }
---
+++ src/shared/components/TopBar/TopBar.tsx
@@ -0,0 +1,25 @@
+import { HOME } from "../../../application/routes/paths";
+
+const links = [
+  {
+    href: HOME,
+    children: "Home",
+  },
+];
+
+export function TopBar() {
+  return (
+    <div>
+      <nav>
+        {links.map(({ href, children }) => (
+          <a key={children} href={href}>
+            {children}
+          </a>
+        ))}
+      </nav>
+      <div>
+        <h1>oi</h1>
+      </div>
+    </div>
+  );
+}
