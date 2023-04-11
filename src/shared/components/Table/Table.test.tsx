cfa50a09514dab8a3a071bca7a0ca5c29eb396fc
Ricnaga <ric.nagato@gmail.com>
2023-04-11 10:13:48 -0300

    refactor(Table): Refatorar teste de tabela

--- src/shared/components/Table/Table.test.tsx
+++ src/shared/components/Table/Table.test.tsx
@@ -1,38 +1,42 @@
 import { Td } from '@chakra-ui/react';
 
-import { render, userEvent, within } from '@application/test/testing-library';
+import {
+  render,
+  screen,
+  userEvent,
+  within,
+} from '@application/test/testing-library';
 
 import { Table, TableHeaderKeys } from './Table';
 
-const header: TableHeaderKeys<{ id: string }> = [
-  { header: 'id', title: 'Mock id' },
-];
+type MockData = { id: string };
 
-const data: Array<{ id: string }> = [
-  { id: 'id1' },
-  { id: 'id1' },
-  { id: 'id2' },
-];
+const header: TableHeaderKeys<MockData> = [{ header: 'id', title: 'Mock id' }];
+
+const data: Array<MockData> = [{ id: 'id1' }, { id: 'id1' }, { id: 'id2' }];
+
+let containerElement: HTMLElement;
 
 describe('Component: Table', () => {
-  it('should render correctly', () => {
+  beforeEach(() => {
     const { container } = render(
       <Table header={header} data={data}>
         {({ id }) => <Td>{id}</Td>}
       </Table>,
     );
-    expect(container).toMatchSnapshot();
+
+    containerElement = container;
+  });
+
+  it('should render correctly', () => {
+    expect(containerElement).toMatchSnapshot();
   });
 
   it('should order column', async () => {
-    const { getByRole } = render(
-      <Table header={header} data={data}>
-        {({ id }) => <Td>{id}</Td>}
-      </Table>,
-    );
-    const columnheader = getByRole('columnheader', {
+    const columnheader = screen.getByRole('columnheader', {
       name: /mock id/i,
     });
+
     const columnButton = within(columnheader).getByRole('button', {
       name: /order icon/i,
     });
@@ -41,6 +45,7 @@ describe('Component: Table', () => {
     await userEvent.click(columnButton);
     await userEvent.click(columnButton);
     await userEvent.click(columnButton);
+
     expect(data[0].id).toBe('id2');
   });
 });
